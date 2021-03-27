0a7b5c0e1bc15852d2e8d2e0292240adb5255f9f
曾詩翰 <gooddennis13@gmail.com>
2021-03-27 10:43:15 +0800

    測試僅計算長邊項目功能

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -20,7 +20,7 @@ public class Main {
         int gain = 0;  //計算gain次數(待check檢查總次數)
         ArrayList<LinkedList<LinkedList<String>>> checkPartList;
 
-        String inputFileName = "tmp.txt";  //輸入測試檔案名稱
+        String inputFileName = "test.txt";  //輸入測試檔案名稱
         final HashMap<String, LinkedList<String>> trajectoryData = getTrajectoryData(inputFileName);  //取得軌跡資料檔案(txt) EX:{t4=[a2, a3, b1], t5=[a3, a1, b1], t6=[a3, a1, b1], t7=[a3, b2, a1], t8=[a3, b2, b3], t1=[a1, b2, b3], t2=[b1, a2, b2, a3], t3=[a2, b3, a3]}
 
         long createBipartiteGraphStartTime = System.currentTimeMillis();   //獲取建立bipartite graph的開始時間
@@ -84,7 +84,7 @@ public class Main {
         BufferedReader reader = null;
 
         try {
-            reader = new BufferedReader(new InputStreamReader(new FileInputStream(".\\input\\" + inputFileName), StandardCharsets.UTF_8)); // 指定讀取文件的編碼格式，以免出現中文亂碼
+            reader = new BufferedReader(new InputStreamReader(new FileInputStream("./input/" + inputFileName), StandardCharsets.UTF_8)); // 指定讀取文件的編碼格式，以免出現中文亂碼
             String str;
 
             while ((str = reader.readLine()) != null) {
@@ -124,17 +124,12 @@ public class Main {
 
         // 掃描所有關聯
         checkPartList.forEach((checkPart) -> {
-            HashSet<String> tmp_chang_t = new HashSet<>();  //暫存 PruningStrategy1不須計算的t項目
-            checkPart.forEach((checkPartKey) -> {
-                if (!checkPartKey.isEmpty()) {
-                    tmp_chang_t.addAll(bipartiteData.biT.get(checkPartKey));
-                    if (bipartiteData.biCT.get(checkPartKey) != null) {
-                        //第二層位置所對應到的關聯位置包含的t
-                        bipartiteData.biCT.get(checkPartKey).forEach((correspondingCheckPart) -> tmp_chang_t.addAll(bipartiteData.biT.get(correspondingCheckPart)));
-                    }
-                }
-            });
-
+            //暫存 PruningStrategy1不須計算的t項目
+            HashSet<String> tmp_chang_t = new HashSet<>(bipartiteData.biT.get(checkPart.get(0)));
+            if (bipartiteData.biCT.get(checkPart.get(0)) != null) {
+                //第二層位置所對應到的關聯位置包含的t
+                bipartiteData.biCT.get(checkPart.get(0)).forEach((correspondingCheckPart) -> tmp_chang_t.addAll(bipartiteData.biT.get(correspondingCheckPart)));
+            }
             chang_t.put(checkPart, tmp_chang_t);
         });
 
